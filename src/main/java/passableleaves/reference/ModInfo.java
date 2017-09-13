eb2f761429d51b78775c3b928ebde4677dc39eb5
WhichOnesPink <whichonespink@hokecraft.co.uk>
2017-09-13 21:53:44 +0100

    Updated to MC 1.12.1

--- src/main/java/passableleaves/reference/ModInfo.java
+++ src/main/java/passableleaves/reference/ModInfo.java
@@ -6,7 +6,7 @@ public class ModInfo
     public static final String MOD_ID = "passableleaves";
     public static final String MOD_NAME = "Passable Leaves";
     public static final String MOD_VERSION = "@MOD_VERSION@";
-    public static final String MC_VERSION = "1.12";
+    public static final String MC_VERSION = "1.12.1";
     public static final String MCF_MINVER = "0.0-MCF+MINVER";
     public static final String MCF_MAXVER = "9001.0-MCF+MAXVER";
     public static final String CONFIG_DIRECTORY = "PassableLeaves";
